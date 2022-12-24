6f34d2510242cd96eb748e3bfb887af519605c70
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2022-12-24 23:56:27 +0530

    Components Updated

---
+++ src/components/FeedBackCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeedBackCard = () => {
+  return (
+    <div>
+      FeedBackCard
+    </div>
+  )
+}
+
+export default FeedBackCard
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from './Navbar'
+import Hero from './Hero'
+import Stats from './Stats'
+import Business from './Business'
+import Billing from './Billing'
+import CardDeal from './CardDeal'
+import Client from './Client'
+import CTA from './CTA'
+import Footer from './Footer'
+import Button from './Button'
+import FeedBackCard from './FeedBackCard'
+import GetStarted from './GetStarted'
+import Testimonials from './Testimonials'
+
+export {
+    Navbar,
+    Hero,
+    Stats,
+    Business,
+    Billing,
+    CardDeal,
+    Client,
+    CTA,
+    Footer,
+    Button,
+    FeedBackCard,
+    GetStarted,
+    Testimonials,
+}
