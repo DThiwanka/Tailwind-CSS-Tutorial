6ff6410ade61dc36a5bfa0b9de6d121fcb170982
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2023-01-01 23:44:13 +0530

    Testimonials initiate

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import {feedback} from '../constants';
+import styles from '../styles';
+import FeedbackCard from '../FeedbackCard'
 
-const Testimonials = () => {
-  return (
-    <div>
-      Testimonials
-    </div>
+const Testimonials = () => (
+  
+    <section id="clients" className='{`${styles.paddingY} ${styles.flexCenter} flex-col relative`}' >
+
+      <div/>
+      
+      <div>
+
+      </div>
+      
+    </section>
+  
   )
-}
 
 export default Testimonials
